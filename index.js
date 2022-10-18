fa55193ba2544ec06880c69d6dd99abaaf00060e
Igor Lamas <igorlamas@fitec.org.br>
2022-10-18 18:14:33 -0300

    feat | types and resolvers for coins

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { Coins, sequelize } = require('./models')
 const port = '4000';
 
 // typeDefs and resolvers
-
 const typeDefs = gql`
   type Coin { name: String, symbol: String, price: String, imageUrl: String, favorite: Boolean }
   type Query { 
@@ -17,20 +16,71 @@ const typeDefs = gql`
   }
 `;
 
-//  resolve map
 
+// map model attribute and make a select before
+const mapAttributes = (model, {fieldNodes}) => {
+  const columns = new Set(Object.keys(model.rawAttributes));
+  const requested_attributes = fieldNodes[0].selectionSet.selections
+    .map(({name: {value}}) => value);
+
+  return requested_attributes.filter(attribute => columns.has(attribute))
+}
+
+//  resolve map
 const resolvers = {
   Query: {
-    coins: async (_, {offset = 0, limit = 10}) => {
+    coins: async (_, { offset = 0, limit = 10 }, context, info) => {
       const coins = await Coins.findAll({
         limit,
         offset,
-        order: [['id', 'ASC']]
+        order: [["id", "ASC"]],
+        attributes: mapAttributes(Coins, info),
       });
       return coins;
-    }
-  }
-}
+    },
+    favorites: async (parent, args, context, info) => {
+      const coins = await Coins.findAll({
+        where: { favorite: true },
+        attributes: mapAttributes(Coins, info),
+      });
+      return coins;
+    },
+  },
+  Mutation: {
+    addCoin: async (_, { symbol }, context, info) => {
+      const [updated] = await Coins.update(
+        { favorite: true },
+        {
+          where: { symbol: symbol },
+        }
+      );
+      if (updated) {
+        const updatedCoin = await Coins.findOne({
+          where: { symbol: symbol },
+          attributes: mapAttributes(Coins, info),
+        });
+        return updatedCoin;
+      }
+      throw new Error("Coin not updated to true");
+    },
+    removeCoin: async (_, { symbol }, context, info) => {
+      const [updated] = await Coins.update(
+        { favorite: false },
+        {
+          where: { symbol: symbol },
+        }
+      );
+      if (updated) {
+        const updatedCoin = await Coins.findOne({
+          where: { symbol: symbol },
+          attributes: mapAttributes(Coins, info),
+        });
+        return updatedCoin;
+      }
+      throw new Error("Coin not updated to false");
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
